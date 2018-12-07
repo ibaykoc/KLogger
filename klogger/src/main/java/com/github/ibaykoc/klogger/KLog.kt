df824deab0ae8f61a0eeaf838a1d5a4d78471343
Mochammad Iqbal <qblmchmmd@gmail.com>
2018-12-07 11:02:11 +0700

    Set is debug on runtime

--- klogger/src/main/java/com/github/ibaykoc/klogger/KLog.kt
+++ klogger/src/main/java/com/github/ibaykoc/klogger/KLog.kt
@@ -7,28 +7,40 @@ class KLog {
     companion object {
         private const val defaultStackLevelUp = 6
 
+        var isDebug = true
         var includeLineLocation = true
+
         fun error(clazz: KClass<*>, message: String, stackLevelUp: Int = defaultStackLevelUp) {
-            if (BuildConfig.DEBUG)
-                System.out.println("KLOG:E [${clazz.simpleName}${if (includeLineLocation) ", ${getFileLocation(stackLevelUp)}" else ""}]: $message")
+            if (isDebug)
+                System.out.println(
+                    "KLOG:E [${clazz.simpleName}${if (includeLineLocation) ", ${getFileLocation(
+                        stackLevelUp
+                    )}" else ""}]: $message"
+                )
         }
 
         fun error(tag: String, message: String, stackLevelUp: Int = defaultStackLevelUp) {
-            if (BuildConfig.DEBUG)
+            if (isDebug)
                 System.out.println("KLOG:E [$tag${if (includeLineLocation) ", ${getFileLocation(stackLevelUp)}" else ""}]: $message")
         }
 
         fun log(clazz: KClass<*>, message: String, stackLevelUp: Int = defaultStackLevelUp) {
-            if (BuildConfig.DEBUG)
-                System.out.println("KLOG:D [${clazz.simpleName}${if (includeLineLocation) ", ${getFileLocation(stackLevelUp)}" else ""}]: $message")
+            if (isDebug)
+                System.out.println(
+                    "KLOG:D [${clazz.simpleName}${if (includeLineLocation) ", ${getFileLocation(
+                        stackLevelUp
+                    )}" else ""}]: $message"
+                )
         }
 
         fun log(tag: String, message: String, stackLevelUp: Int = defaultStackLevelUp) {
-            if (BuildConfig.DEBUG)
+            if (isDebug)
                 System.out.println("KLOG:D [$tag${if (includeLineLocation) ", ${getFileLocation(stackLevelUp)}" else ""}]: $message")
         }
 
-        private fun getFileLocation(stackLevelUp: Int): String = "(${getFileName(stackLevelUp)}:${getLineNumber(stackLevelUp)})"
+        private fun getFileLocation(stackLevelUp: Int): String =
+            "(${getFileName(stackLevelUp)}:${getLineNumber(stackLevelUp)})"
+
         private fun getLineNumber(stackLevelUp: Int): Int = Thread.currentThread().stackTrace[stackLevelUp].lineNumber
         private fun getFileName(stackLevelUp: Int): String = Thread.currentThread().stackTrace[stackLevelUp].fileName
     }
